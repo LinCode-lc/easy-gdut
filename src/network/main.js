7b6d49dc15902aff3b753852c346e12f99374a69
linweifan <1763977795@qq.com>
2021-03-24 08:40:16 +0800

    登录页

---
+++ src/network/main.js
@@ -0,0 +1,22 @@
+import {request} from './request'
+
+export function getList(pageNo, size, tab) {
+    return request(({
+      url: '/post/list',
+      method: 'get',
+      params: { pageNo: pageNo, size: size, tab: tab }
+    }))
+  }
+
+// export class Goods {
+// 	constructor(itemInfo, columns, services) {
+// 		this.title = itemInfo.title
+// 		this.desc = itemInfo.desc
+// 		this.newPrice = itemInfo.price
+// 		this.oldPrice = itemInfo.oldPrice
+// 		this.discount = itemInfo.discountDesc
+// 		this.columns = columns
+// 		this.services = services
+// 		this.realPrice = itemInfo.lowNowPrice
+// 	}
+// }
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="main">
+        
         <el-col :xs="24" :sm="5">
           <Card></Card>
         </el-col>
@@ -23,18 +24,62 @@
         <el-col :xs="24" :sm="5">
           <Card></Card>
         </el-col>
+
+        <pagination
+        v-show="page.total > 0"
+        :total="page.total"
+        :page.sync="page.current"
+        :limit.sync="page.size"
+        @pagination="init"
+      />
     </div>
 </template>
 
 <script>
 import Card from "../components/content/Card"
 import Card2 from "../components/content/Card2"
+import { getList } from '@/network/main.js'
+import Pagination from "@/components/common/Pagination"
 export default {
     name:"Main",
     components:{
     Card,
-    Card2
+    Card2,
+    Pagination
+    },
+     data() {
+        return {
+            activeName: 'latest',
+            articleList: [],
+            page: {
+            current: 1,
+            size: 10,
+            total: 0,
+            tab: 'latest'
+           
+      }
     }
+  },
+    created() {
+    this.init(this.tab)
+    
+  },
+  methods: {
+    init(tab) {
+      getList(this.page.current, this.page.size, tab).then((response) => {
+        const { data } = response
+        this.page.current = data.current
+        this.page.total = data.total
+        this.page.size = data.size
+        this.articleList = data.records
+      
+      })
+    },
+    // handleClick(tab) {
+    //   this.page.current = 1
+    //   this.init(tab.name)
+    // }
+  }
 }
 </script>
 
